8b6a8a5c68b88eb31c3d1a6ad43a80745c8505a8
swapnilP <swapnil.p@amuratech.com>
2016-09-17 14:37:36 +0530

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 5.times do |t|
-
   name=Faker::Pokemon.name
   category=rand(0..5)
   date=Faker::Date.forward(rand(0..30))
@@ -17,25 +16,28 @@
 
   t=Test.new(category:category,name:name,date:date,duration:duration,marks:marks,number_of_questions:number_of_questions)
   t.save
-
 end
-#
-# 10.times do |t|
-#   question="This is sample Question"
-#   question_type=rand(0..2)
-#
-#   if question_type==0
-#     options=[]
-#     correct_answer=[Faker::Color.color_name]
-#   else
-#     options=[Faker::Color.color_name,Faker::Color.color_name,Faker::Color.color_name,Faker::Color.color_name]
-#     if question_type==1
-#       correct_answer=[options[rand(0..4)]]
-#     else
-#       correct_answer=[options[1],options[3]]
-#     end
-#   end
-#
-#   q=Question.new(question_type:question_type,question:question,options:options.join('###'),correct_answer:correct_answer.join('###'))
-#   q.save
-# end
+
+10.times do |t|
+  question="This is sample Question"
+  question_type=rand(0..2)
+
+  if question_type==0
+    options=[]
+    correct_answer=[Faker::Color.color_name]
+  else
+    options=[Faker::Color.color_name,Faker::Color.color_name,Faker::Color.color_name,Faker::Color.color_name]
+    if question_type==1
+      correct_answer=[options[rand(0..4)]]
+    else
+      correct_answer=[options[1],options[3]]
+    end
+  end
+
+  q=Question.new(question_type:question_type,question:question,options:options.join('###'),correct_answer:correct_answer.join('###'))
+  q.save
+end
+
+
+Test.first.questions=Question.take(5)
+Test.last.questions=Question.take(5)
