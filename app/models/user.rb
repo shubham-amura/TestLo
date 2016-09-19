096fabffc5e0e9caa37b479ba4ded24bc8a075fd
shubham-amura <shubham@amuratech.com>
2016-09-19 17:11:54 +0530

    Validations added to user and migration deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username, uniqueness: true
+  validates :phone_no , uniqueness: true, format: { with: /[0-9]/,message: "Must contain only numbers"},
+  				     length: { minimum: 10 ,message: "Must be 10 digits"}
 end
--- db/migrate/20160917073104_add_student_id_to_student_details.rb
+++
@@ -1,5 +0,0 @@
-class AddStudentIdToStudentDetails < ActiveRecord::Migration
-  def change
-    add_column :student_details, :student_id, :integer
-      end
-end
