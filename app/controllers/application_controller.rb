372d61a2b7890d875f9944225020b41e9d7aba99
shubham-amura <shubham@amuratech.com>
2016-09-19 18:40:12 +0530

    Profile Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    profile_path(resource)
+  end
+
+
+  def authorize_user!
+    redirect_to profile_path(current_user) unless current_user.id.to_i == params[:id].to_i
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,:confirmable
   validates :username, uniqueness: true
   validates :phone_no , uniqueness: true, format: { with: /[0-9]/,message: "Must contain only numbers"},
   				     length: { minimum: 10 ,message: "Must be 10 digits"}
-=======
-         :recoverable, :rememberable, :trackable, :validatable,:confirmable
->>>>>>> b206283cecaee9813a0b9c4b73e11c33902afe40
+
 end
