2cfee8d4ba7cce2ac94969faf96c3591484ce9f7
swapnilP <swapnil.p@amuratech.com>
2016-09-20 19:17:05 +0530

    javascript for dynamic form

--- app/assets/javascripts/qform.js
+++ app/assets/javascripts/qform.js
@@ -1,7 +1,45 @@
+$(document).ready(function(){
 
-function a()
+    toggleOptions();
+    toggelCorrectOptions();
+});
+
+function toggleOptions()
 {
+  var options=$('#options');
+  var a0=$('#correct_answers_0');
+  var a1=$('#correct_answers_1');
+  var a2=$('#correct_answers_2');
 
-alert('Hello')
+  $('#question_question_type_0').change(function(){
+    options.toggle(!options.is(':visible'));
+    a0.show();
+    a1.hide();
+    a2.hide();
+  });
 
+  $('#question_question_type_1').change(function(){
+      if(options.is(':hidden'))
+      {
+        options.show();
+      }
+      a1.show();
+      a0.hide();
+      a2.hide();
+  });
+
+
+  $('#question_question_type_2').change(function(){
+      if(options.is(':hidden'))
+      {
+        options.show();
+      }
+      a2.show();
+      a0.hide();
+      a1.hide();
+  });
+}
+
+function toggelCorrectOptions()
+{
 }
