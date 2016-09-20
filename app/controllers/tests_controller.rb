0b2406fca8f143651f29f70b333ac4830dae6fa2
swapnilP <swapnil.p@amuratech.com>
2016-09-20 12:14:37 +0530

    temporary links to dashboard,question resources

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
-
-
-
+  def new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get 'profile/:id' => 'profiles#show' , as: :profile
 
   resources :tests
+  resources :questions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
