072c793a06770f0311efc1f6336cd7ed0f09c620
swapnilP <swapnil.p@amuratech.com>
2016-09-20 17:49:12 +0530

    question create form modified

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :array_to_string,only:[:create]
+  #to check params are valid depending on
   def new
     @question=Question.new
   end
@@ -8,7 +10,22 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    p params
+    #options=params[:question][:temp_options].join('#####')
+    #@user=Question.new(question_params)
+    question_params
+    render plain: params
   end
 
+  private
+
+  def question_params
+    params.require(:question).permit(:question_type,:question,:options,:correct_answer,)
+  end
+
+  def array_to_string
+    if params[:question][:correct_answer].is_a?Array
+      params[:question][:correct_answer]=params[:question][:correct_answer].join('#####')
+    end
+    params[:question][:options]=params[:question][:temp_options].join('#####')
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,5 @@
 class Question < ActiveRecord::Base
-  attr_accessor :options_hash
-
+  
   has_many :test_questions
   has_many :tests ,through: :test_questions
-
-  @options_hash=[]
 end
