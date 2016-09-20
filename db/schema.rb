f4b96ff30332daa9c762dc04411536d6cebf98bd
swapnilP <swapnil.p@amuratech.com>
2016-09-20 17:53:38 +0530

    category model added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919064151) do
+ActiveRecord::Schema.define(version: 20160920122244) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employer_details", force: :cascade do |t|
     t.string   "company"
