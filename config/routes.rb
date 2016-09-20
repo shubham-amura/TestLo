68b903df84a6940c496dacb8aedf132ee2ad680b
shubham-amura <shubham@amuratech.com>
2016-09-20 18:37:18 +0530

    Home page and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+
+root 'static_pages#home'
+
   #devise_for :users
 
   devise_for :users,controllers: { sessions: 'users/sessions' ,passwords: 'users/passwords',
