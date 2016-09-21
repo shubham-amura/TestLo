ba3cb57873a0ae036725ab2ec98dda935cfae70e
swapnilP <swapnil.p@amuratech.com>
2016-09-21 15:14:10 +0530

    routes

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -10,9 +10,12 @@ class TestsController < ApplicationController
   end
 
   def create
-    @test=current_user.tests.create(test_params)
-    render plain: @test.inspect
-    #@test=current_user.tests.build(test_params)
+    @test=current_user.tests.build(test_params)
+    render 'add_questions'
+    
+    # @test=current_user.tests.create(test_params)
+    # render plain: @test.inspect
+    # #@test=current_user.tests.build(test_params)
     # render plain: params
     # if current_user.tests.create(test_params)
     #   flash[:success]="Test created Successfully"
@@ -21,7 +24,6 @@ class TestsController < ApplicationController
     #   render 'new'
     # end
   end
-
   private
   def test_params
     params.require(:test).permit(:name,:date,:duration)
