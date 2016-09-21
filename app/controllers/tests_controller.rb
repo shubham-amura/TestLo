6d0cc05ca8660dd4267142a5e7f6b3debbe66d01
swapnilP <swapnil.p@amuratech.com>
2016-09-21 14:43:38 +0530

    test duration/date,index added

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,10 +1,29 @@
 class TestsController < ApplicationController
+
+  def index
+    @tests=Test.all
+  end
+
   def new
-    @test=current_user.tests.build()
+    @test=Test.new
+    # render plain: @test.inspect
   end
 
   def create
-
+    @test=current_user.tests.create(test_params)
+    render plain: @test.inspect
+    #@test=current_user.tests.build(test_params)
+    # render plain: params
+    # if current_user.tests.create(test_params)
+    #   flash[:success]="Test created Successfully"
+    #   redirect_to profile_path(current_user)
+    # else
+    #   render 'new'
+    # end
   end
 
+  private
+  def test_params
+    params.require(:test).permit(:name,:date,:duration)
+  end
 end
