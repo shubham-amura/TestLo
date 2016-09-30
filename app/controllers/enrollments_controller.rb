370c1b57a2b260bd45201545deb1ffc0970b1579
shubham-amura <shubham@amuratech.com>
2016-09-30 17:50:30 +0530

    Marks functionality adde

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -5,22 +5,24 @@ class EnrollmentsController < ApplicationController
   def taketest
 
     @current_test = Test.find(params[:test_id])
-    temp=TestQuestion.all.where(test_id:params[:test_id]).pluck(:question_id)
-    @questions = Question.where(id:temp)
+    @temp=TestQuestion.all.where(test_id:params[:test_id]).pluck(:question_id,:marks)
+    @questions = Question.where(id:@temp.map{|a,b| a})
     @current_question = @questions.first
+
   end
 
 
   def show_current_question
 
     @current_question = Question.find(params[:id])
-
-
     respond_to do |format|
+      format.js
+  end
+  end
 
-    format.js
-
-    end
+  def submit_clicked
 
   end
+  
+
 end
