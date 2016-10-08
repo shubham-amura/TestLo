2eee117c747fcbe890e2b369235ea8c49b8ef3be
shubham-amura <shubham@amuratech.com>
2016-10-08 10:59:33 +0530

    Test refactor

--- app/models/test.rb
+++ app/models/test.rb
@@ -10,9 +10,7 @@ class Test < ActiveRecord::Base
   has_many :enrollments,dependent: :destroy
   has_many :students ,through: :enrollments
 
-
   #validaions
-
   #testname validation
   validates :name ,
             uniqueness: true,
@@ -27,21 +25,16 @@ class Test < ActiveRecord::Base
 
   validate :duration_greater_than_zero
 
-
   def duration_greater_than_zero
     if self.duration <= 0
       errors.add(:duration, "can't be zero")
     end
   end
 
-
-
-
-
   def test_date_cannot_be_in_the_past
-        if date.present? && date < DateTime.now.utc.to_date
-                errors.add(:date, "can't be in the past")
-        end
+    if date.present? && date < DateTime.now.utc.to_date
+          errors.add(:date, "can't be in the past")
+    end
   end
 
 end
