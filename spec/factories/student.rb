8de15ffcba1f4dc7bf7c82e76ddc2e2b602680a6
shubham-amura <shubham@amuratech.com>
2016-10-10 20:59:10 +0530

    Profile Controller Spec Done

---
+++ spec/factories/student.rb
@@ -0,0 +1,12 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :student do |f|
+
+    f.email    {  Faker::Internet.email        }
+    f.name     {  Faker::Name.name             }
+    f.phone_no {  Faker::PhoneNumber.cell_phone}
+    f.username { "djbsfbsi"  }
+    f.password {  Faker::Internet.password     }
+  end
+end
