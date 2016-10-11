685ce3fb10ade2414847aae5126563dfba5c3d84
swapnilP <swapnil.p@amuratech.com>
2016-10-11 16:49:51 +0530

    params in test controller fixed

--- spec/controllers/tests_controller_spec.rb
+++ spec/controllers/tests_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe TestsController, type: :controller do
 
   describe "POST" do
     it "creats new test" do
-      params = {"name"=>"a", "date(1i)"=>"2016", "date(2i)"=>"10", "date(3i)"=>"10", "duration(1i)"=>"2016", "duration(2i)"=>"10", "duration(3i)"=>"10", "duration(4i)"=>"12", "duration(5i)"=>"57"}
+      params = {"name"=>"a", "date(1i)"=>Time.now.utc.year, "date(2i)"=>Time.now.utc.month, "date(3i)"=>Time.now.utc.day, "duration(1i)"=>"2016", "duration(2i)"=>"10", "duration(3i)"=>"10", "duration(4i)"=>"12", "duration(5i)"=>"57"}
        expect { post :create, test: params }.to change(Test, :count).by(1)
     end
   end
@@ -29,8 +29,9 @@ RSpec.describe TestsController, type: :controller do
     end
 
     it "Shows test" do
-      params = {"name"=>"a", "date(1i)"=>"2016", "date(2i)"=>"10", "date(3i)"=>"10", "duration(1i)"=>"2016", "duration(2i)"=>"10", "duration(3i)"=>"10", "duration(4i)"=>"12", "duration(5i)"=>"57"}
+      params = {"name"=>"a", "date(1i)"=>Time.now.utc.year, "date(2i)"=>Time.now.utc.month, "date(3i)"=>Time.now.utc.day, "duration(1i)"=>"2016", "duration(2i)"=>"10", "duration(3i)"=>"10", "duration(4i)"=>"12", "duration(5i)"=>"57"}
       post :create, test: params
+
       current_test = @employer_user.tests.first
       get :show ,id: current_test.id
       expect(response.status).to eq (200)
@@ -39,7 +40,7 @@ RSpec.describe TestsController, type: :controller do
 
   describe "DELETE" do
     it "Delete existinf test" do
-      params = {"name"=>"a", "date(1i)"=>"2016", "date(2i)"=>"10", "date(3i)"=>"10", "duration(1i)"=>"2016", "duration(2i)"=>"10", "duration(3i)"=>"10", "duration(4i)"=>"12", "duration(5i)"=>"57"}
+      params = {"name"=>"a", "date(1i)"=>Time.now.utc.year, "date(2i)"=>Time.now.utc.month, "date(3i)"=>Time.now.utc.day, "duration(1i)"=>"2016", "duration(2i)"=>"10", "duration(3i)"=>"10", "duration(4i)"=>"12", "duration(5i)"=>"57"}
       post :create, test: params
       current_test = @employer_user.tests.first
       expect { delete :destroy , id:current_test.id }.to change(Test, :count).by(-1)
